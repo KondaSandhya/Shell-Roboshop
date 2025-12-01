48464923413358ac2318dc03bdcb92c850b57b28
Sandhya <sandhyareddy5e4@gmail.com>
2025-12-01 12:25:21 +0530

    Creating ec2 instance

--- roboshop.sh
+++ roboshop.sh
@@ -5,5 +5,5 @@ AMI_ID="ami-09c813fb71547fc4f"
 INSTANCE_TYPE="t3.micro"
 SG="sg-0b3869c561a82bbce"
 
-INSTANCE_ID="$(aws ec2 run instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG --count 1 --query "Instances[0].InstanceId" --output text)"
+INSTANCE_ID="$(aws ec2 run instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro  --security-group-ids sg-0b3869c561a82bbce --count 1 --query "Instances[0].InstanceId" --output text)"
 echo "Instance created successfully with instance id: $INSTANCE_ID"
