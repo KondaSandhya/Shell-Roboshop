de4539a909fae9d772b7f7ebc8a3855321bd92f1
Sandhya <sandhyareddy5e4@gmail.com>
2025-12-01 12:03:18 +0530

    Creating ec2 instance

---
+++ roboshop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#creating the ec2 instances automatically through script using aws cli
+AMI_ID="ami-09c813fb71547fc4f"
+INSTANCE_TYPE="t3.micro"
+SG="sg-0b3869c561a82bbce"
+
+INSTANCE_ID="$(aws run instances --image-id $AMI_ID --instance-type $INSTANCE_TYPE --security-group-ids $SG --tags Key=Name,Value=Shell-roboshop  --count 1 --query 'Instances[0].InstanceId' --output text)"
+echo "Instance created successfully with instance id: $INSTANCE_ID"
